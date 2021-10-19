3fac5fdfcd1059529054e41a8313ac2d209f7cf0
MZidar <MZidar@gmail.com>
2021-10-19 13:11:44 -0400

    commit tree 7c3c86c01c4516eae8af807e7b07a0daa2c997a6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+
+    def clickbait
+        unless !!title && title.match(/Top \d|Won't Believe|Secret|Guess/)
+            errors.add(:title, "Sorry, need clicks bro.")
+        end
+    
+    end
+
 end
